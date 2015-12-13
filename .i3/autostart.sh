593f93854512a074b50f7732ccc7311fe29c9261
Thomas Fehér <thomasfeher@web.de>
2015-12-13 22:28:21 +0100

    add autostart of applications using i3 layout saving

---
+++ .i3/autostart.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+# go to a workspace that is not used by autostart applications to avoid wrong workspace name
+i3-msg "workspace 0"
+
+# load i3 layout
+i3-msg "append_layout $HOME/.i3/layout.json"
+
+# load applications
+thunderbird &
+firefox &
+urxvt -e screen &
+dolphin &
+weechat &
